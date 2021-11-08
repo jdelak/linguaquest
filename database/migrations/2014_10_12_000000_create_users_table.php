8995ad0e01affd9b1c78be1a97aa7df869222354
jdelak <delakdev@gmail.com>
2021-11-09 00:36:29 +0100

    move project to laravel

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 16)->unique();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->enum('gender', array('female', 'male', 'other'));
+            $table->integer('level')->default(1);
+            $table->integer('gem')->default(0);
+            $table->boolean('premium')->default(0);
+            $table->dateTime('premium_start_date');
+            $table->dateTime('premium_end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
